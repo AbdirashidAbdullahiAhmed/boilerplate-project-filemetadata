1fdac976935b9b8135d82f858aec771d8092c23a
TaHa <madax361@gmail.com>
2024-08-16 11:59:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+const upload = multer({dest: './public/data/uploads'})
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'), (req, res)=>{
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  // Send response with file details
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+})
 
 
 
